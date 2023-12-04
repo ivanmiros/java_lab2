05bde543457f11e9dd310e36026d5f0f6e2b049f
ivanmiros <ivanmiros@gmail.com>
2023-12-04 22:58:34 +0100

    Add step 3

--- src/main/java/matrix/Matrix.java
+++ src/main/java/matrix/Matrix.java
@@ -3,26 +3,26 @@ package matrix;
 
 public class Matrix {
  
-    private int[][] _data = null;
+    private double[][] _data = null;
     private int _x=0, _y=0;
 
     public Matrix(int x, int y) {
         this._x = x;
         this._y = y;
-        _data = new int [this._x][this._y];
+        _data = new double [this._x][this._y];
     }
 
     public Matrix() {
         this._x = 0;
         this._y = 0;
-        _data = new int [this._x][this._y];
+        _data = new double [this._x][this._y];
     }    
 
     public Matrix(Matrix m_other) {
         this._data = m_other._data.clone();
         this._x = m_other._x;
         this._y = m_other._y;        
-        _data = new int [this._x][this._y];
+        _data = new double [this._x][this._y];
     }    
 
     public int GetSize() {
@@ -35,8 +35,6 @@ public class Matrix {
             output = "[]\n";
             return output;
         }
-
-        
         for (int j=0; j<this._y; j++) {
             output += "[ ";
             for (int i=0; i<this._x; i++) {
@@ -44,14 +42,37 @@ public class Matrix {
             }
             output +="]\n";
         }
-
         return output;
     }
 
-    
+    public void fillCell (int x, int y, double val) {
+        if (x <=0 || x > this._x || y <= 0 || y > this._y ) {
+            throw new IllegalArgumentException("Row or Column numbers are wrong for this Matrix");
+        }
+        this._data[x-1][y-1] = val;
+    }    
+
+    public void fillX (int y, double... val) {
+        if ( val.length != this._x ) {
+            throw new IllegalArgumentException("Row (X) count values is wrong for this Matrix");
+        } 
+        for (int i = 0; i < val.length; i++){
+            this.fillCell(i+1, y, val[i]);
+        }
+    }
+
+    public void fillY (int x, double... val) {
+        if ( val.length != this._y ) {
+            throw new IllegalArgumentException("Column (Y) count values is wrong for this Matrix");
+        } 
+        for (int i = 0; i < val.length; i++){
+            this.fillCell(x, i+1, val[i]);
+        }
+    }    
 
     public static void main(String args[]){
-        Matrix m = new Matrix(2, 2);
+        Matrix m = new Matrix(4, 2);
+        m.fillY(1, 2.1, 2.2);
         //System.out.println("Matrix size: "+ m.GetSize());
         System.out.println(m.Druc());
     }
--- src/test/java/matrix/matrixTest.java
+++ src/test/java/matrix/matrixTest.java
@@ -38,7 +38,34 @@ public class matrixTest {
     @Test
     public void checkDruc2x2() {
         Matrix m1 = new Matrix(2,2);
-        assertEquals("[ 0 0 ]\n[ 0 0 ]\n", m1.Druc());
+        assertEquals("[ 0.0 0.0 ]\n[ 0.0 0.0 ]\n", m1.Druc());
+    }  
+
+    @Test
+    public void checkWrongFillCell() {
+        Matrix m1 = new Matrix(2,2);
+        Exception exception = assertThrows(IllegalArgumentException.class,()-> m1.fillCell(-1, 1, 1.5));
+    }      
+
+    @Test
+    public void checkCorrectFillCell() {
+        Matrix m1 = new Matrix(2,2);
+        m1.fillCell(1, 1, 1.5);
+        assertEquals("[ 1.5 0.0 ]\n[ 0.0 0.0 ]\n",m1.Druc());
+    }  
+
+    @Test
+    public void checkFillRow() {
+        Matrix m1 = new Matrix(4,2);
+        m1.fillX(1, 1.1, 1.2, 1.3, 1.4);
+        assertEquals("[ 1.1 1.2 1.3 1.4 ]\n[ 0.0 0.0 0.0 0.0 ]\n",m1.Druc());
+    }  
+    
+    @Test
+    public void checkFillColumn() {
+        Matrix m1 = new Matrix(4,2);
+        m1.fillY(1, 2.1, 2.2);
+        assertEquals("[ 2.1 0.0 0.0 0.0 ]\n[ 2.2 0.0 0.0 0.0 ]\n",m1.Druc());
     }  
 
 }
